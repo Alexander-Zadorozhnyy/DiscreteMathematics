da90781a04c09c514d0241137dff7a2fb6d03b2b
Alexander-Zadorozhnyy <sasha-zadorozhnyy@mail.ru>
2022-03-21 18:51:18 +0300

    Refactor structure

--- Практика 24.03.22/Task 3.py
+++
@@ -1,6 +0,0 @@
-from Task_4_5.useful_functions_to_work_with_project import get_average_lenght
-from Task_4_5.ShannonFano_algorithm import get_ShannonFano_codes
-
-ShannonFano_codes = get_ShannonFano_codes(['A', 'B', 'C', 'D'], [0.5, 0.3, 0.1, 0.1])
-print(get_average_lenght([0.5, 0.3, 0.1, 0.1], list(ShannonFano_codes.values())))
-# Answer B) 1.7
